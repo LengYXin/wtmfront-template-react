33eb0753f2b1ab70ad96c4b9d39b3702c97c7e09
LengYingxin <lengyingxin8966@gmail.com>
2018-10-10 10:14:37 +0800

    update

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,5 +1,3 @@
 export default {
     editer: () => import('./editer').then(x => x.default),
-    test: () => import('./test').then(x => x.default),
-    testAAAAAAAAAAAAA: () => import('./testAAAAAAAAAAAAA').then(x => x.default)
 }
